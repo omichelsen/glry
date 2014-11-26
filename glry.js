e2baa5f61cde34bc69a6ccbde1efd04ca297f59c
Ole Bjørn Michelsen <ole@michelsen.dk>
2014-11-27 00:01:43 +0100

    Minor fix to tapping.

--- glry.js
+++ glry.js
@@ -1,5 +1,5 @@
 /*!
- * glry v0.3.2 (https://github.com/omichelsen/glry)
+ * glry v0.3.3 (https://github.com/omichelsen/glry)
  * Copyright 2014 Ole Michelsen <ole@michelsen.dk>
  * Licensed under MIT
  */
@@ -232,6 +232,7 @@
         }
 
         function handleNavigationToggle(e) {
+            e.preventDefault();
             elmNavigation.style.opacity = Number(elmNavigation.style.opacity) ? 0 : 1;
         }
 
