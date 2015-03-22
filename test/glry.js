696904ec28c42c047cffeb350fa83ded8475081a
Ole Bjørn Michelsen <ole@michelsen.dk>
2015-03-22 13:58:39 +0100

    Changed tests to run async due to animations.

--- test/glry.js
+++ test/glry.js
@@ -10,7 +10,13 @@ describe('glry', function () {
         return elm;
     }
 
-    beforeEach(function () {
+    function createEvent(eventName) {
+        var e = document.createEvent('MouseEvents');
+        e.initMouseEvent(eventName, true, true, window, 0, 0, 0, 0, 0, false, false, false, false, 0, null);
+        return e;
+    }
+
+    beforeAll(function () {
         elm = createElement('figure', {'id': 'figure'});
         elm.appendChild(createElement('div', {'class': 'loading'}));
         elm.appendChild(createElement('div', {'class': 'error'}));
@@ -21,6 +27,7 @@ describe('glry', function () {
         document.body.appendChild(elm);
 
         options = {
+            animationSpeed: 0,
             load: function (direction) {
                 if (direction === 'left')
                     return 'prev-image.jpg';
@@ -28,19 +35,39 @@ describe('glry', function () {
                     return 'next-image.jpg';
             }
         };
-
-        spyOn(options, 'load');
-
-        glry = new Glry(options);
     });
 
-    afterEach(function () {
+    beforeEach(function () {
+        spyOn(options, 'load').and.callThrough();
+    });
+
+    afterAll(function () {
         elm.parentNode.removeChild(elm);
         elm = null;
     });
 
-    it('should load image', function () {
-        expect(options.load).toHaveBeenCalled();
+    it('should load image on init', function () {
+        glry = new Glry(options);
+        expect(options.load).toHaveBeenCalledWith(undefined);
+    });
+
+    it('should show error', function (done) {
+        options.onLoadEnd = function () {
+            expect(elm.querySelector('.error').style.display).toEqual('block');
+            done();
+        };
+        glry = new Glry(options);
+    });
+
+    it('should load next', function (done) {
+        options.onLoadEnd = function () {
+            var next = elm.querySelector('.next');
+            next.dispatchEvent(createEvent('mouseup'));
+            expect(options.load.calls.count()).toEqual(2);
+            expect(options.load).toHaveBeenCalledWith('right');
+            done();
+        };
+        glry = new Glry(options);
     });
 
 });
