e0fc9af04b20cb28177f9345345529a84ba22471
Ole Bjørn Michelsen <ole@michelsen.dk>
2015-03-22 13:56:55 +0100

    Use Firefox for tests as PhantomJS doesn't support CustomEvent or .bind().

--- karma.conf.js
+++ karma.conf.js
@@ -54,7 +54,7 @@ module.exports = function(config) {
     // - Safari (only Mac)
     // - PhantomJS
     // - IE (only Windows)
-    browsers: ['PhantomJS'],
+    browsers: ['Firefox'],
 
     // If browser does not capture in given timeout [ms], kill it
     captureTimeout: 60000,
